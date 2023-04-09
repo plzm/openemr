011e0adf5e83b3ed78fdc6be089be1fb7bcc2c5c
Patrick El-Azem <p@elazem.com>
2023-04-09 10:10:00 -0400

    Initial scaffolding

---
+++ scripts/create-service-principal-for-github-actions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+
+# This script is provided so you can create an Azure Service Principal (SP) and copy its output to a Github repo secret.
+# This SP will be used to execute Azure commands in workflows.
+
+subscriptionName="ALFAADIN"
+servicePrincipalName="sp-aa-openemr"
+
+subscriptionId=$(echo "$(az account show -s $subscriptionName -o tsv --query 'id')" | sed "s/\r//")
+#echo $subscriptionId | cat -v
+
+# Create a Service Principal with Owner role on the subscription
+az ad sp create-for-rbac --name $servicePrincipalName --role Owner --scopes /subscriptions/$subscriptionId --verbose --sdk-auth
+
+# Capture the output of the above to a GitHub repo secret named AZURE_CREDENTIALS
---
+++ scripts/set-env-vars.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ##################################################
+# NOTE - in non-GitHub environment, to work with the env vars exported herein from other files, remember to dot-source this file at the prompt!
+# . ./env-vars.sh
+# ##################################################
+
+setEnvVar() {
+	# Set an env var's value at runtime with dynamic variable name
+	# If in GitHub Actions runner, will export env var both to Actions and local shell
+  # Usage:
+  # setEnvVar "variableName" "variableValue"
+
+  varName=$1
+  varValue=$2
+
+	if [[ ! -z $GITHUB_ACTIONS ]]
+	then
+		# We are in GitHub CI environment - export to GitHub Actions workflow context for availability in later tasks in this workflow
+		cmd=$(echo -e "echo \x22""$varName""=""$varValue""\x22 \x3E\x3E \x24GITHUB_ENV")
+		eval $cmd
+	fi
+
+	# Export for local/immediate use, whether on GHA runner or shell/wherever
+	cmd="export ""$varName""=\"""$varValue""\""
+	eval $cmd
+}
+
