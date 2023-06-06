fcd44653a0405dfeb21b9ea18fe7f0d8f6f1fa87
Patrick El-Azem <p@elazem.com>
2023-06-06 07:28:43 -0400

    NSG work

--- scripts/infra/Network.ps1
+++ scripts/infra/Network.ps1
@@ -1,5 +1,4 @@
-function DeployNetwork()
-{
+function DeployNetwork() {
   [CmdletBinding()]
   param
   (
@@ -25,34 +24,57 @@ function DeployNetwork()
 
   Write-Debug -Debug:$true -Message "Deploy Network"
 
+  $nsgIndex = 1
+
+  foreach ($nsg in $configMatrix.Network.NSGs) {
+    $nsgName = GetResourceName -ConfigAll $configAll -ConfigMatrix $configMatrix -Prefix "nsg" -Sequence ($nsgIndex.ToString().PadLeft(2, "0"))
+
+    DeployNSG `
+      -SubscriptionID "$SubscriptionId" `
+      -Location $configMatrix.Location `
+      -ResourceGroupName $rgName `
+      -TemplateUri ($configAll.TemplateUriPrefix + "net.nsg.json") `
+      -NSGName $nsgName `
+      -Tags $Tags
+
+    foreach ($nsgRule in $nsg) {
+      DeployNSGRule `
+      -SubscriptionID "$SubscriptionId" `
+      -Location $configMatrix.Location `
+      -ResourceGroupName $rgName `
+      -TemplateUri ($configAll.TemplateUriPrefix + "net.nsg.rule.json") `
+      -NSGName $nsgName `
+      -Tags $Tags
+    }
+  }
+
+
   $vnetIndex = 1
 
-  foreach ($vnet in $configMatrix.Network.VNets)
-  {
+  foreach ($vnet in $configMatrix.Network.VNets) {
     $vnetName = GetResourceName -ConfigAll $configAll -ConfigMatrix $configMatrix -Prefix "vnt" -Sequence ($vnetIndex.ToString().PadLeft(2, "0"))
 
     DeployVNet `
-    -SubscriptionID "$SubscriptionId" `
-    -Location $configMatrix.Location `
-    -ResourceGroupName $rgName `
-    -TemplateUri ($configAll.TemplateUriPrefix + "net.vnet.json") `
-    -VNetName $vnetName `
-    -VNetPrefix $configMatrix.Network.AddressSpace `
-    -EnableDdosProtection $false `
-    -Tags $Tags
-
-    foreach ($subnet in $vnet.Subnets)
-    {
-      DeploySubnet `
       -SubscriptionID "$SubscriptionId" `
+      -Location $configMatrix.Location `
       -ResourceGroupName $rgName `
-      -TemplateUri ($configAll.TemplateUriPrefix + "net.vnet.subnet.json") `
+      -TemplateUri ($configAll.TemplateUriPrefix + "net.vnet.json") `
       -VNetName $vnetName `
-      -SubnetName $subnet.Name `
-      -SubnetPrefix $subnet.AddressSpace `
-      -NsgResourceId "" `
-      -RouteTableResourceId "" `
-      -DelegationService $subnet.Delegation
+      -VNetPrefix $configMatrix.Network.AddressSpace `
+      -EnableDdosProtection $false `
+      -Tags $Tags
+
+    foreach ($subnet in $vnet.Subnets) {
+      DeploySubnet `
+        -SubscriptionID "$SubscriptionId" `
+        -ResourceGroupName $rgName `
+        -TemplateUri ($configAll.TemplateUriPrefix + "net.vnet.subnet.json") `
+        -VNetName $vnetName `
+        -SubnetName $subnet.Name `
+        -SubnetPrefix $subnet.AddressSpace `
+        -NsgResourceId "" `
+        -RouteTableResourceId "" `
+        -DelegationService $subnet.Delegation
     }
 
     $vnetIndex++
@@ -60,8 +82,96 @@ function DeployNetwork()
 
 }
 
-function DeployVNet()
-{
+function DeployNSG() {
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $NSGName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
+  )
+
+  Write-Debug -Debug:$true -Message "Deploy NSG"
+
+  az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$NSGName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    location="$Location" `
+    nsgName="$NSGName" `
+    tags=$Tags
+}
+
+function DeployNSGRule() {
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $NSGName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $NSGRuleName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
+  )
+
+  Write-Debug -Debug:$true -Message "Deploy NSG Rule"
+
+  az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$NSGRuleName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    nsgName="$NSGName" `
+    nsgRuleName="$NSGRuleName" `
+    description="$Description" `
+    priority="$Priority" `
+    direction="$Direction" `
+    access="$Access" `
+    protocol="$Protocol" `
+    sourceAddressPrefix="$SourceAddressPrefix" `
+    sourceAddressPrefixes="$SourceAddressPrefixes" `
+    sourcePortRange="$SourcePortRange" `
+    sourcePortRanges="$SourcePortRanges" `
+    destinationAddressPrefix="$DestinationAddressPrefix" `
+    destinationAddressPrefixes="$DestinationAddressPrefixes" `
+    destinationPortRange="$DestinationPortRange" `
+    destinationPortRanges="$DestinationPortRanges"
+}
+
+function DeployVNet() {
   [CmdletBinding()]
   param
   (
@@ -110,8 +220,7 @@ function DeployVNet()
     tags=$Tags
 }
 
-function DeploySubnet()
-{
+function DeploySubnet() {
   [CmdletBinding()]
   param
   (
