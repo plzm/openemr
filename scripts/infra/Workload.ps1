f00626646947aa90192105688302755e806165ba
Patrick El-Azem <p@elazem.com>
2023-08-09 16:16:54 -0400

    Add workload

--- scripts/infra/Workload.ps1
+++ scripts/infra/Workload.ps1
@@ -18,6 +18,24 @@ function Deploy-AppServicePlan()
     [Parameter(Mandatory = $true)]
     [string]
     $AppServicePlanName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SkuName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SkuTier,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SkuFamily,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Capacity,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Kind,
+    [Parameter(Mandatory = $false)]
+    [bool]
+    $ZoneRedundant = $true,
     [Parameter(Mandatory = $false)]
     [string]
     $Tags = ""
@@ -33,6 +51,12 @@ function Deploy-AppServicePlan()
     --parameters `
     location="$Location" `
     appServicePlanName="$AppServicePlanName" `
+    skuName="$SkuName" `
+    skuTier="$SkuTier" `
+    skuFamily="$SkuFamily" `
+    capacity="$Capacity" `
+    kind="$Kind" `
+    zoneRedundant="$ZoneRedundant" `
     tags=$Tags
 }
 
@@ -56,6 +80,18 @@ function Deploy-AppServicePlanAutoscaleSettings()
     [Parameter(Mandatory = $true)]
     [string]
     $AutoscaleSettingsName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AppServicePlanResourceId,
+    [Parameter(Mandatory = $true)]
+    [int]
+    $MinimumInstances,
+    [Parameter(Mandatory = $true)]
+    [int]
+    $MaximumInstances,
+    [Parameter(Mandatory = $true)]
+    [int]
+    $DefaultInstances,
     [Parameter(Mandatory = $false)]
     [string]
     $Tags = ""
@@ -71,6 +107,60 @@ function Deploy-AppServicePlanAutoscaleSettings()
     --parameters `
     location="$Location" `
     autoScaleSettingsName="$AutoscaleSettingsName" `
+    appServicePlanResourceId="$AppServicePlanResourceId" `
+    minimumInstances="$MinimumInstances" `
+    maximumInstances="$MaximumInstances" `
+    defaultInstances="$DefaultInstances" `
+    tags=$Tags
+}
+
+function Deploy-AppServiceCertificate()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AppServicePlanResourceId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AppServiceCertificateName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $KeyVaultResourceId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $KeyVaultSecretName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
+  )
+
+  Write-Debug -Debug:$true -Message "Deploy App Service Certificate"
+
+  az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$AppServiceCertificateName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    location="$Location" `
+    appServicePlanResourceId="$AppServicePlanResourceId" `
+    certificateName="$AppServiceCertificateName" `
+    keyVaultResourceId="$KeyVaultResourceId" `
+    keyVaultSecretName="$KeyVaultSecretName" `
     tags=$Tags
 }
 
@@ -94,6 +184,63 @@ function Deploy-AppService()
     [Parameter(Mandatory = $true)]
     [string]
     $AppServiceName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Kind,
+    [Parameter(Mandatory = $false)]
+    [bool]
+    $AssignSystemIdentity = $false,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $UserAssignedIdentityResourceId = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $UserAssignedIdentityClientId = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $AppServicePlanResourceId = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $AppInsightsResourceId = "",
+    [Parameter(Mandatory = $true)]
+    [string]
+    $StorageAccountName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $FunctionRuntimeVersion = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $FunctionRuntimeWorker = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Language = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $LinuxFxVersion = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $DotnetVersion = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PythonVersion = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PublicNetworkAccess = "Disabled",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $SubnetResourceId = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $RouteAllTrafficThroughVNet = $true,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $AllowedIpAddressRanges = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $CustomFqdn = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $CertificateForAppServiceThumbprint = "",
     [Parameter(Mandatory = $false)]
     [string]
     $Tags = ""
@@ -109,6 +256,25 @@ function Deploy-AppService()
     --parameters `
     location="$Location" `
     appServiceName="$AppServiceName" `
+    kind="$Kind" `
+    assignSystemIdentity="$AssignSystemIdentity" `
+    userAssignedIdentityResourceId="$UserAssignedIdentityResourceId" `
+    userAssignedIdentityClientId="$UserAssignedIdentityClientId" `
+    appServicePlanResourceId="$AppServicePlanResourceId" `
+    appInsightsResourceId="$AppInsightsResourceId" `
+    storageAccountName="$StorageAccountName" `
+    functionRuntimeVersion="$FunctionRuntimeVersion" `
+    functionRuntimeWorker="$FunctionRuntimeWorker" `
+    language="$Language" `
+    linuxFxVersion="$LinuxFxVersion" `
+    dotnetVersion="$DotnetVersion" `
+    pythonVersion="$PythonVersion" `
+    publicNetworkAccess="$PublicNetworkAccess" `
+    subnetResourceId="$SubnetResourceId" `
+    routeAllTrafficThroughVNet="$RouteAllTrafficThroughVNet" `
+    allowedIpAddressRanges="$AllowedIpAddressRanges" `
+    customFqdn="$CustomFqdn" `
+    certificateForAppServiceThumbprint="$CertificateForAppServiceThumbprint" `
     tags=$Tags
 }
 
@@ -132,6 +298,18 @@ function Deploy-AppInsights()
     [Parameter(Mandatory = $true)]
     [string]
     $AppInsightsName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $LogAnalyticsWorkspaceResourceId,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $LinkedStorageAccountResourceId = "",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PublicNetworkAccessForIngestion = "Enabled",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PublicNetworkAccessForQuery = "Enabled",
     [Parameter(Mandatory = $false)]
     [string]
     $Tags = ""
@@ -147,5 +325,9 @@ function Deploy-AppInsights()
     --parameters `
     location="$Location" `
     appInsightsName="$AppInsightsName" `
+    logAnalyticsWorkspaceResourceId="$LogAnalyticsWorkspaceResourceId" `
+    linkedStorageAccountResourceId="$LinkedStorageAccountResourceId" `
+    publicNetworkAccessForIngestion="$PublicNetworkAccessForIngestion" `
+    publicNetworkAccessForQuery="$PublicNetworkAccessForQuery" `
     tags=$Tags
 }
