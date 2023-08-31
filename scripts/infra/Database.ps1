5fbe63a3c7a7801985b0454978311053be1d42b4
Patrick El-Azem <p@elazem.com>
2023-08-31 07:30:11 -0400

    Fix database instance name

--- scripts/infra/Database.ps1
+++ scripts/infra/Database.ps1
@@ -102,29 +102,3 @@ function Deploy-MariaDbServer()
 
   return $output
 }
-
-function Get-DatabaseInstanceName()
-{
-  [CmdletBinding()]
-  param
-  (
-    [Parameter(Mandatory = $true)]
-    [object]
-    $ConfigConstants,
-    [Parameter(Mandatory = $true)]
-    [object]
-    $ConfigGlobal,
-    [Parameter(Mandatory = $true)]
-    [string]
-    $Suffix
-  )
-
-  $instanceName = Get-ResourceName `
-    -ConfigConstants $ConfigConstants `
-    -ConfigGlobal $ConfigGlobal `
-    -Prefix $ConfigGlobal.PrefixDatabase `
-    -Sequence $configConstants.SeqNumData `
-    -Suffix $Suffix
-
-  return $instanceName
-}
